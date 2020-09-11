78eb85c45e8c39f221f4310a45fc3c00e508d6aa
Joyce <joyce_barros@yahoo.com.br>
2020-09-10 22:02:57 -0300

    Criando a classe ResolucaoDeConflitosGit

---
+++ banco-recode/src/com/br/recode/git/ResolucaoDeConflitosGit.java
@@ -0,0 +1,21 @@
+package com.br.recode.git;
+
+public class ResolucaoDeConflitosGit {
+	
+	private String conflito1;
+	private String conflito2;
+	private String conflito3;
+	
+	public String getConflito1() {
+		return conflito1;
+	}
+	public String getConflito2() {
+		return conflito2;
+	}
+	public String getConflito3() {
+		return conflito3;
+	}
+	
+	
+
+}
