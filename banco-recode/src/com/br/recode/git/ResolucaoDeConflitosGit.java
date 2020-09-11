890ac9534848b76a6ef800621d1d10e4e819a0ae
joycebarros <59984799+joycebarros@users.noreply.github.com>
2020-09-10 22:08:02 -0300

    Inserindo o setConflito1

--- banco-recode/src/com/br/recode/git/ResolucaoDeConflitosGit.java
+++ banco-recode/src/com/br/recode/git/ResolucaoDeConflitosGit.java
@@ -3,12 +3,17 @@ package com.br.recode.git;
 public class ResolucaoDeConflitosGit {
 	
 	private String conflito1;
-	private String conflito2;
+	private String conflito22;
 	private String conflito3;
 	
 	public String getConflito1() {
 		return conflito1;
 	}
+	
+	public void setConflito1(String conflito1) {
+		this.conflito1 = conflito1;
+	}
+	
 	public String getConflito2() {
 		return conflito2;
 	}
